f9dafa174c3dafc456441d102b651f458ebda913
Igor Yagushevsky <yagushevskij@gmail.com>
2020-10-30 18:08:09 +0300

    removed IIFE wrapper

--- src/index.js
+++ src/index.js
@@ -19,45 +19,43 @@ import { NewsCardList } from './scripts/components/NewsCardList';
 import { MainApi } from './scripts/api/MainApi';
 import { NewsApi } from './scripts/api/NewsApi';
 
-(() => {
-  'use strict';
-  const mainApi = new MainApi(config.mainApi);
-  const newsApi = new NewsApi(config.newsApi);
-  const getUserData = () => mainApi.getUserData();
-  const header = new Header(headerContainer, getUserData,
-    {
-      page: config.header.pages.index,
-      menuLinks: config.header.menuLinks,
-      text: config.header.text,
-      menuLinkTemplate: headerMenuLinkTemplate,
-      menuButtonTemplate: headerButtonTemplate,
-    });
-  const createNewsCard = (...args) => new NewsCard(newsCardTemp, mainApi,
-    config.card.pages.index).create(...args);
-  const newsCardList = new NewsCardList(searchResultContainer, createNewsCard,
-    {
-      isLoadingTemp, notFoundTemp, srvErrTemp, cardsBlockTemp,
-    },
-    config.cardList.pages.index);
-  const search = new Search(searchForm, newsCardList, newsApi.getNews, getUserData, config.search);
+'use strict';
+const mainApi = new MainApi(config.mainApi);
+const newsApi = new NewsApi(config.newsApi);
+const getUserData = () => mainApi.getUserData();
+const header = new Header(headerContainer, getUserData,
+  {
+    page: config.header.pages.index,
+    menuLinks: config.header.menuLinks,
+    text: config.header.text,
+    menuLinkTemplate: headerMenuLinkTemplate,
+    menuButtonTemplate: headerButtonTemplate,
+  });
+const createNewsCard = (...args) => new NewsCard(newsCardTemp, mainApi,
+  config.card.pages.index).create(...args);
+const newsCardList = new NewsCardList(searchResultContainer, createNewsCard,
+  {
+    isLoadingTemp, notFoundTemp, srvErrTemp, cardsBlockTemp,
+  },
+  config.cardList.pages.index);
+const search = new Search(searchForm, newsCardList, newsApi.getNews, getUserData, config.search);
 
-  const createForm = (form, backendErrorEl, ...args) => new Form(
-    form, backendErrorEl, config.form, ...args,
-  );
+const createForm = (form, backendErrorEl, ...args) => new Form(
+  form, backendErrorEl, config.form, ...args,
+);
 
-  const signinPopup = new SigninPopup(loginPopupTemplate, popupContainer, mainApi.login, createForm,
-    header.render, newsCardList);
-  const signupPopup = new SignupPopup(signupPopupTemplate, popupContainer, mainApi.register,
-    createForm);
-  const informPopup = new InformPopup(successPopupTemplate, popupContainer);
+const signinPopup = new SigninPopup(loginPopupTemplate, popupContainer, mainApi.login, createForm,
+  header.render, newsCardList);
+const signupPopup = new SignupPopup(signupPopupTemplate, popupContainer, mainApi.register,
+  createForm);
+const informPopup = new InformPopup(successPopupTemplate, popupContainer);
 
-  const logout = () => mainApi.logout();
-  const openSigninPopup = () => signinPopup.open();
+const logout = () => mainApi.logout();
+const openSigninPopup = () => signinPopup.open();
 
-  informPopup.setDependencies({ signinPopup });
-  signinPopup.setDependencies({ signupPopup });
-  signupPopup.setDependencies({ informPopup, signinPopup });
-  header.setDependencies({ openSigninPopup, logout });
+informPopup.setDependencies({ signinPopup });
+signinPopup.setDependencies({ signupPopup });
+signupPopup.setDependencies({ informPopup, signinPopup });
+header.setDependencies({ openSigninPopup, logout });
 
-  search.addEventListener('submit', search.submit);
-})();
+search.addEventListener('submit', search.submit);
--- src/saved-news/index.js
+++ src/saved-news/index.js
@@ -13,28 +13,26 @@ import { NewsCardList } from '../scripts/components/NewsCardList';
 import { SavedNews } from '../scripts/components/SavedNews';
 import { MainApi } from '../scripts/api/MainApi';
 
-(() => {
-  'use strict';
-  const mainApi = new MainApi(config.mainApi);
-  const getUserData = () => mainApi.getUserData();
-  const getSavedNews = () => mainApi.getArticles();
-  const header = new Header(headerContainer, getUserData,
-    {
-      page: config.header.pages.savedNews,
-      menuLinks: config.header.menuLinks,
-      text: config.header.text,
-      menuLinkTemplate: headerMenuLinkTemplate,
-      menuButtonTemplate: headerButtonTemplate,
-    });
-  const createNewsCard = (...args) => new NewsCard(newsCardTemp, mainApi,
-    config.card.pages.savedNews).create(...args);
-  const newsCardList = new NewsCardList(searchResultContainer, createNewsCard,
-    {
-      isLoadingTemp, notFoundTemp, srvErrTemp, cardsBlockTemp,
-    },
-    config.cardList.pages.savedNews);
-  const logout = () => mainApi.logout();
-  header.setDependencies({ logout });
-  const savedNews = new SavedNews(savedNewsContainer, newsCardList, getSavedNews, getUserData);
-  savedNews.renderResults();
-})();
+'use strict';
+const mainApi = new MainApi(config.mainApi);
+const getUserData = () => mainApi.getUserData();
+const getSavedNews = () => mainApi.getArticles();
+const header = new Header(headerContainer, getUserData,
+  {
+    page: config.header.pages.savedNews,
+    menuLinks: config.header.menuLinks,
+    text: config.header.text,
+    menuLinkTemplate: headerMenuLinkTemplate,
+    menuButtonTemplate: headerButtonTemplate,
+  });
+const createNewsCard = (...args) => new NewsCard(newsCardTemp, mainApi,
+  config.card.pages.savedNews).create(...args);
+const newsCardList = new NewsCardList(searchResultContainer, createNewsCard,
+  {
+    isLoadingTemp, notFoundTemp, srvErrTemp, cardsBlockTemp,
+  },
+  config.cardList.pages.savedNews);
+const logout = () => mainApi.logout();
+header.setDependencies({ logout });
+const savedNews = new SavedNews(savedNewsContainer, newsCardList, getSavedNews, getUserData);
+savedNews.renderResults();
